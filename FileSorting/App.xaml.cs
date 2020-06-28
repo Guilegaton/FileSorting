eeccab92fe08f09a53d564208e1f11819ae09dd2
Julius <yulii.vedkal@nure.ua>
2020-06-28 16:45:14 +0300

    Add project files.

---
+++ FileSorting/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileSorting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Call with Startup property in App.xaml
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AppStartupMainMAGI(object sender, StartupEventArgs e)
+        {
+            String[] arguments = Environment.GetCommandLineArgs();
+
+            if (arguments.GetLength(0) > 1)
+            {
+                if (arguments[1].EndsWith(".magi"))
+                {
+                    string filePathFormMainArgs = arguments[1];
+                    MessageBox.Show("BOOM!");
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
